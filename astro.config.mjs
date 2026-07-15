d1c4ab4815b326ab54ab91fa2605ca7fd6cefaf3
pbrocart <pbrocart@cochlear.com>
2026-07-15 14:18:09 +0200

    Configuration de la base url pour GitHub Pages

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // L'adresse complète de votre site GitHub Pages
+  site: 'https://ouchinou.github.io',
+  // Le nom exact de votre dépôt GitHub précédé et suivi d'un slash
+  base: '/methode-musique/',
+});
